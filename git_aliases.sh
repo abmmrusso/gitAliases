dc0a3bb60d5e856fc6b132134e6f0e6c1c52327d
abmmrusso <abmmrusso@users.noreply.github.com>
2019-01-01 22:48:53 +0000

    Git batch repo update logic added with gitUpdateAll README updated
    Aliases transformed to functions

--- git_aliases.sh
+++ git_aliases.sh
@@ -1,1 +1,38 @@
-alias gitGoTo='if [ -z ${GIT_PROJECT_FOLDER+x} ] ; then echo "GIT_PROJECT_FOLDER variable is not defined."; elif [ ! -d "$GIT_PROJECT_FOLDER" ] ; then echo "GIT_PROJECT_FOLDER defined location does not exist or is not accessible."; else cd ${GIT_PROJECT_FOLDER}; fi'
+function gitGoTo {
+  if [ -z ${GIT_PROJECT_FOLDER+x} ] ;
+  then
+    echo "GIT_PROJECT_FOLDER variable is not defined.";
+    return -1;
+  elif [ ! -d "$GIT_PROJECT_FOLDER" ] ;
+  then
+    echo "GIT_PROJECT_FOLDER defined location does not exist or is not accessible.";
+    return -2;
+  else
+    cd ${GIT_PROJECT_FOLDER};
+  fi
+  return 0
+}
+
+function gitUpdateAll {
+  gitGoTo
+  if [[ $? != 0 ]]
+  then
+    return -1
+  fi
+
+  for d in `ls -d */`
+  do
+    cd $d
+    if [[ "$(git rev-parse --is-inside-work-tree 2> /dev/null)" == "true" ]]
+    then
+      if [[ "$(git rev-parse --abbrev-ref $(git rev-parse --abbrev-ref HEAD)@{upstream} > /dev/null 2>&1 || echo not_tracked)" == "not_tracked" ]]
+      then
+        echo "Skipping update of $(basename $d) as current branch ($(git rev-parse --abbrev-ref HEAD)) is not set up for remote tracking."
+      else
+        echo "Updating $(basename $d)"
+        git pull
+      fi
+    fi
+  done
+  return 0
+}
--- tests/git_aliases_test.sh
+++ tests/git_aliases_test.sh
@@ -1,37 +1,128 @@
 #!/bin/bash -i
 
 BASE_PROJECT_FOLDER=$(cd $(dirname "${BASH_SOURCE[0]}")/.. ; pwd)
+#BUILD_PROJECT_FOLDER=${BASE_PROJECT_FOLDER}/build
+BUILD_PROJECT_FOLDER=~/.gitAliasTesting
 
 . ${BASE_PROJECT_FOLDER}/git_aliases.sh
 
-setUp() {
+###
+# Auxiliary functions
+###
+cleanupTestGitProjectFolder() {
   cd $BASE_PROJECT_FOLDER
-  GIT_PROJECT_FOLDER=${BASE_PROJECT_FOLDER}/build
-  rm -rf $GIT_PROJECT_FOLDER
-  mkdir $GIT_PROJECT_FOLDER
+  rm -rf $BUILD_PROJECT_FOLDER
+}
+
+setUpTestGitProjectFolder() {
+  cleanupTestGitProjectFolder
+  mkdir -p $GIT_PROJECT_FOLDER
+}
+
+###
+# Setup for each testcase
+###
+setUp() {
+  GIT_PROJECT_FOLDER=${BUILD_PROJECT_FOLDER}/gitRepos
 }
 
 tearDown() {
-  cd $BASE_PROJECT_FOLDER
-  rm -rf $GIT_PROJECT_FOLDER
+  cleanupTestGitProjectFolder
 }
 
+###
+# Test cases for gitGoTo
+###
 testGitGoToAliasEchoesErrorIfNoGitProjectFolderVariableDefined() {
   unset GIT_PROJECT_FOLDER
-  local alias_output="$(gitGoTo)"
-  assertEquals "GIT_PROJECT_FOLDER variable is not defined." "${alias_output}"
+  local git_go_to_output="$(gitGoTo)"
+  assertEquals "GIT_PROJECT_FOLDER variable is not defined." "${git_go_to_output}"
 }
 
 testGitGoToAliasEchoesErrorIfGitProjectFolderLocationDoesNotExist() {
-  GIT_PROJECT_FOLDER=${GIT_PROJECT_FOLDER}/doesNotExist
-  local alias_output="$(gitGoTo)"
-  assertEquals "GIT_PROJECT_FOLDER defined location does not exist or is not accessible." "${alias_output}"
+  local git_go_to_output="$(gitGoTo)"
+  assertEquals "GIT_PROJECT_FOLDER defined location does not exist or is not accessible." "${git_go_to_output}"
 }
 
 testGitGoToAliasMovesUserToGitFolderVariableHeldLocation() {
-  GIT_PROJECT_FOLDER=${GIT_PROJECT_FOLDER}
+  setUpTestGitProjectFolder
+
   gitGoTo
   assertEquals "${GIT_PROJECT_FOLDER}" "$(pwd)"
+}
+
+###
+# Test cases for gitUpdateAll
+###
+testGitUpdateAllAliasEchoesErrorIfNoGitProjectFolderVariableDefined() {
+  unset GIT_PROJECT_FOLDER
+  local git_update_output="$(gitUpdateAll)"
+  assertEquals "GIT_PROJECT_FOLDER variable is not defined." "${git_update_output}"
+}
+
+testGitUpdateAllAliasEchoesErrorIfGitProjectFolderLocationDoesNotExist() {
+  local git_update_output="$(gitUpdateAll)"
+  assertEquals "GIT_PROJECT_FOLDER defined location does not exist or is not accessible." "${git_update_output}"
+}
+
+testGitUpdateAllAliasShouldIgnoreNonGitReposInGitProjectFolder() {
+  setUpTestGitProjectFolder
+
+  mkdir -p ${GIT_PROJECT_FOLDER}/notAGitRepo
+
+  local git_update_output="$(gitUpdateAll)"
+
+  assertEquals "" "${git_update_output}"
+}
+
+testGitUpdateAllAliasShouldNotPullUpdatesForGitRepositoriesWithNoRemoteForCurrentBranch() {
+  setUpTestGitProjectFolder
+
+  mkdir -p ${GIT_PROJECT_FOLDER}/localOnlyGitRepo
+  (cd ${GIT_PROJECT_FOLDER}/localOnlyGitRepo &&
+   git init &&
+   echo "This is a test README" >> ${GIT_PROJECT_FOLDER}/localOnlyGitRepo/README.MD &&
+   git add . &&
+   git commit -m 'Test Readme added') > /dev/null 2>&1
+
+  local git_update_output="$(gitUpdateAll)"
+
+  assertEquals "Skipping update of localOnlyGitRepo as current branch (master) is not set up for remote tracking." "${git_update_output}"
+}
+
+testGitUpdateAllAliasUpdatesAllGitRepositoriesInGitProjectFolder() {
+  setUpTestGitProjectFolder
+
+  mkdir -p ${BUILD_PROJECT_FOLDER}/testGitRepoRemote
+  (cd ${BUILD_PROJECT_FOLDER}/testGitRepoRemote;
+   git init;
+   echo "This is a test README" >> ${BUILD_PROJECT_FOLDER}/testGitRepoRemote/README.MD;
+   git add .;
+   git commit -m 'Test Readme added') > /dev/null 2>&1
+
+  cp -r ${BUILD_PROJECT_FOLDER}/testGitRepoRemote ${GIT_PROJECT_FOLDER}/gitUpdateableRepo
+  (cd ${GIT_PROJECT_FOLDER}/gitUpdateableRepo;
+   git remote add origin ${BUILD_PROJECT_FOLDER}/testGitRepoRemote;
+   git fetch;
+   git branch --set-upstream-to origin/master master) > /dev/null 2>&1
+
+  (cd ${BUILD_PROJECT_FOLDER}/testGitRepoRemote;
+   echo nonsense >> .gitignore;
+   git add .gitignore;
+   git commit -m 'Git ignore added';) > /dev/null 2>&1
+
+  local expected_upstream_commit_hash=$(cd ${BUILD_PROJECT_FOLDER}/testGitRepoRemote; git rev-parse HEAD)
+  echo $expected_upstream_commit_hash
+
+  (cd ${GIT_PROJECT_FOLDER}/gitUpdateableRepo; git log $expected_upstream_commit_hash --pretty=format:%H -1) > /dev/null 2>&1
+  assertFalse "TEST SETUP PROBLEM: Repo to update already contains the commit hash expected to show up only after update" "[ $? -eq 0 ]"
+
+  local git_update_output=$(gitUpdateAll)
+
+  (cd ${GIT_PROJECT_FOLDER}/gitUpdateableRepo; git log $expected_upstream_commit_hash --pretty=format:%H -1) > /dev/null 2>&1
+  assertTrue "Repo does not contain expected commit hash after update" "[ $? -eq 0 ]"
+
+  assertContains "Output does not contain expected message about repo being updated" "${git_update_output}" "Updating testGitRepoRemote"
 }
 
 . $(which shunit2)
